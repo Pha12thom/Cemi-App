1f644ccb1995c6635a398a5c932994ae9820a4bf
pha12thom <MilugoGeofrey.Ouma@studentambasssadors.com>
2024-05-14 17:47:20 +0300

    add cart

--- cemi/store/models.py
+++ cemi/store/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
+
 # Create your models here.
+class Store(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    email = models.EmailField()
+    website = models.URLField()
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class items(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
