7455dcab10d4b1db9adfba00533816e42d94c7d1
pha12thom <MilugoGeofrey.Ouma@studentambasssadors.com>
2024-05-14 23:21:39 +0300

    add item details

--- cemi/store/urls.py
+++ cemi/store/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 urlpatterns = [
     path('', views.home, name='home'),
     path('base/', views.base, name='base'),
+    path('details/<int:item_id>/', views.details, name='details'),
+    path('add_to_cart/<int:item_id>/', views.add_to_cart, name='add_to_cart'),
    
 ]
--- cemi/store/views.py
+++ cemi/store/views.py
@@ -26,7 +26,8 @@ def base(request):
     }
     return render(request, 'base.html', context)
 
-def cart(request):
+def details(request, item_id):
+    item = get_object_or_404(items, pk=item_id)
     user_page = user.objects.all()
     store_page = Store.objects.all()
     items_page = items.objects.all()
@@ -34,6 +35,22 @@ def cart(request):
         'user_page': user_page,
         'store_page': store_page,
         'items_page': items_page,
+        'item': item,
+    }
+    
+    return render(request, 'details.html', context)
+
+
+
+def add_to_cart(request, item_id):
+    item = get_object_or_404(items, pk=item_id)
+    user_page = user.objects.all()
+    store_page = Store.objects.all()
+    items_page = items.objects.all()
+    context = {
+        'user_page': user_page,
+        'store_page': store_page,
+        'items_page': items_page,
+        'item': item,
     }
     return render(request, 'cart.html', context)
-
