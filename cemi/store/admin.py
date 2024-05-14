cd690e2c77f549733d9d0aa82c5bf51e79105605
pha12thom <MilugoGeofrey.Ouma@studentambasssadors.com>
2024-05-14 18:03:50 +0300

    create users

--- cemi/store/admin.py
+++ cemi/store/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Store, items
 
+admin.site.register(Store)
+admin.site.register(items)
+# This is the admin.py file where we register our models.
 # Register your models here.
--- cemi/store/models.py
+++ cemi/store/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 
+class user(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.name
+    
+    
 # Create your models here.
 class Store(models.Model):
     name = models.CharField(max_length=100)
