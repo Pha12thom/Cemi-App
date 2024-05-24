c3e386e3c6382905f25588a19048432517f9e330
pha12thom <MilugoGeofrey.Ouma@studentambasssadors.com>
2024-05-24 18:55:38 +0300

    add statocs

--- cemi/store/forms.py
+++ cemi/store/forms.py
@@ -61,3 +61,8 @@ class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
         fields = ['content', 'rating']
+        widgets = {
+            'content': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Write your review here...'}),
+            'rating': forms.NumberInput(attrs={'class': 'form-control', 'min': 1, 'max': 5}),
+        }
+
